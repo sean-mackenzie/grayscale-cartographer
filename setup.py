4ccbe2eb6437c2fe854e94344e881a8b7865cb9a
sean mackenzie <sean_mackenzie@ucsb.edu>
2023-02-18 21:47:01 -0800

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='grayscale-cartographer',
+    version='1.1',
+    packages=['graycart'],
+    url='https://github.com/sean-mackenzie/grayscale-cartographer',
+    license='',
+    author='Sean MacKenzie',
+    author_email='sean_mackenzie@ucsb.edu',
+    description='Package for grayscale lithography design and evaluation.'
+)
